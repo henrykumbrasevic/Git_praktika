587f6e2a58a04ce56fab9e67b2ad2f8c5e74f42d
laima <laima.kulbyte@stud.techin.lt>
2024-12-04 16:03:14 +0400

    add events_closed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import EventsClosed from "./components/EventsClosed";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    
+    <>
+    <EventsClosed/>
+    </>
   );
 }
